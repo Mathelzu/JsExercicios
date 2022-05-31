90664e71c8501ce7f743d0c2d66e748828279732
Matheus <matheusprogramao2003@gmail.com>
2022-05-31 17:46:04 -0400

    Adiciona exercicio da porta

---
+++ aberturaDaPorta.js
@@ -0,0 +1,15 @@
+console.log("Abertura da porta.");
+// software de reconhecimento facial
+
+const rosto = false;
+const portaAberta = true;
+
+if(rosto == true){
+    portaAberta == true
+    console.log("Acesso liberado! Bem vindo");
+}else{
+    portaAberta == false;
+    console.log("Acesso negado! Quem é você?");
+};
+
+
